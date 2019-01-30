e70ce25aae3552f0f90c4b62e85ecf8c206af2c2
Mark Bradshaw <notthehoople@googlemail.com>
2019-01-30 22:20:49 +0000

    A minor draw to a file

--- mandelbrot/mandelbrot.go
+++ mandelbrot/mandelbrot.go
@@ -3,10 +3,60 @@ package main
 import (
 	"flag"
 	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"log"
+	"os"
 )
 
+// Mandelbrot pseudocode
+//
+/*
+For each pixel (Px, Py) on the screen, do:
+{
+  x0 = scaled x coordinate of pixel (scaled to lie in the Mandelbrot X scale (-2.5, 1))
+  y0 = scaled y coordinate of pixel (scaled to lie in the Mandelbrot Y scale (-1, 1))
+  x = 0.0
+  y = 0.0
+  iteration = 0
+  max_iteration = 1000
+  while (x*x + y*y <= 2*2  AND  iteration < max_iteration) {
+    xtemp = x*x - y*y + x0
+    y = 2*x*y + y0
+    x = xtemp
+    iteration = iteration + 1
+  }
+  color = palette[iteration]
+  plot(Px, Py, color)
+}
+*/
+
 func mandelbrotControl(debug bool) {
+	//	p := image.Point{2, 1}
+
+	r := image.Rect(2, 1, 5, 5)
+	fmt.Println(r.Dx(), r.Dy(), image.Pt(0, 0).In(r)) // prints 3 4 false
+
 	fmt.Println("mandelbrotControl")
+
+	m := image.NewRGBA(image.Rect(0, 0, 640, 480))
+	m.Set(5, 5, color.RGBA{255, 0, 0, 255})
+
+	// new code
+	rectangle := "rectangle.png"
+
+	rectImage := image.NewRGBA(image.Rect(0, 0, 200, 200))
+	green := color.RGBA{0, 100, 0, 255}
+
+	draw.Draw(rectImage, rectImage.Bounds(), &image.Uniform{green}, image.ZP, draw.Src)
+
+	file, err := os.Create(rectangle)
+	if err != nil {
+		log.Fatalf("failed create file: %s", err)
+	}
+	png.Encode(file, rectImage)
 }
 
 // Main routine
